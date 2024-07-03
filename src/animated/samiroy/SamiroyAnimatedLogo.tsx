f144eab5d94535fd4b8f91272a5ae360d5f8a5c2
Samir Roy <rsamir@gmail.com>
2024-07-03 12:01:00 +0530

    v1.1.1

--- src/animated/samiroy/SamiroyAnimatedLogo.tsx
+++ src/animated/samiroy/SamiroyAnimatedLogo.tsx
@@ -2,47 +2,45 @@ import * as React from 'react';
 import SamiroyLogo from '../../vector/samiroy/SamiroyLogo';
 import PongAnimation from './PongAnimation';
 
-const SamiroyAnimatedLogo = () => {
-  const styles: Record<string, React.CSSProperties> = {
-    wrapper: {
-      overflow: 'clip',
-      height: 150,
-    },
-    container: {
-      display: 'inline-block',
-      margin: 'auto',
-      width: 'calc(100%)',
-      height: 150,
-      maxWidth: 360,
-    },
-    animation: {
-      position: 'absolute',
-      width: 'calc(100%)',
-      display: 'contents',
-    },
-    logo: {
-      display: 'inline-block',
-      top: -160,
-      position: 'relative',
-    },
-    sprite: {
-      height: 168,
-      width: 145,
-    },
-  };
-
-  return (
-    <div style={styles.wrapper}>
-      <div style={styles.container}>
-        <div style={styles.animation}>
-          <PongAnimation />
-        </div>
-        <div style={styles.logo}>
-          <SamiroyLogo style={styles.sprite} />
-        </div>
+const SamiroyAnimatedLogo = () => (
+  <div style={styles.wrapper}>
+    <div style={styles.container}>
+      <div style={styles.animation}>
+        <PongAnimation />
+      </div>
+      <div style={styles.logo}>
+        <SamiroyLogo style={styles.sprite} />
       </div>
     </div>
-  );
+  </div>
+);
+
+const styles: Record<string, React.CSSProperties> = {
+  wrapper: {
+    overflow: 'clip',
+    height: 150,
+  },
+  container: {
+    display: 'inline-block',
+    margin: 'auto',
+    width: 'calc(100%)',
+    height: 150,
+    maxWidth: 360,
+  },
+  animation: {
+    position: 'absolute',
+    width: 'calc(100%)',
+    display: 'contents',
+  },
+  logo: {
+    display: 'inline-block',
+    top: -138,
+    position: 'relative',
+  },
+  sprite: {
+    height: 128,
+    width: 116,
+  },
 };
 
 export default React.memo(SamiroyAnimatedLogo);
