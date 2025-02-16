c4f93f30a5e41211cd32157f70c691afe85ff906
Samir Roy <rsamir@gmail.com>
2025-02-16 15:25:12 -0500

    v1.1.2 (#4)
    * New animation for samiroy logo

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import PoonjiAnimatedLogo from './animated/poonji/PoonjiAnimatedLogo';
 import SamiroyLogo from './vector/samiroy/SamiroyLogo';
 import SamiroyTitle from './vector/samiroy/SamiroyTitle';
 import SamiroyAnimatedLogo from './animated/samiroy/SamiroyAnimatedLogo';
+import SamiroyAnimatedLogoPong from './animated/samiroy/SamiroyAnimatedLogoPong';
 
 import EndlessTrafficTitle from './vector/traffic/EndlessTrafficTitle';
 import EndlessTrafficAnimatedLogo from './animated/traffic/EndlessTrafficAnimatedLogo';
@@ -19,6 +20,7 @@ export {
   SamiroyLogo,
   SamiroyTitle,
   SamiroyAnimatedLogo,
+  SamiroyAnimatedLogoPong,
   EndlessTrafficTitle,
   EndlessTrafficAnimatedLogo,
 };
